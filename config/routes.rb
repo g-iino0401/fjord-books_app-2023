ce4e48bac12a9fff7354c13abf5caeb5fdacec8d
iino <g-iino@ruby-dev.jp>
2024-03-21 11:54:28 +0900

    deviseを使ってユーザー認証機能とユーザーページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :books
+  devise_for :users
+  root to: 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: %i(index show)
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
