48c27f82c2afd287b1bb2c4b82ce2efd81cb0297
iino <g-iino@ruby-dev.jp>
2024-04-02 12:46:57 +0900

    i18n修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,10 +22,10 @@ class BooksController < ApplicationController
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
-
+  
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: t('notice.create') }
+        format.html { redirect_to book_url(@book), notice: t('notice.create', models: t("activerecord.models.book")) }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: t('notice.update') }
+        format.html { redirect_to book_url(@book), notice: t('notice.update', models: t("activerecord.models.book")) }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,8 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('notice.destroy') }
-      format.json { head :no_content }
+      format.html { redirect_to books_url, notice: t('notice.destroy', models: t("activerecord.models.book")) }
     end
   end
 
